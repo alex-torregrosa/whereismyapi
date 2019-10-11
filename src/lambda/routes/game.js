b6c6d587e22a27a71145e9d596bafd7bf351feb0
Pep Borrell <borrellpep@gmail.com>
2019-10-12 00:46:51 +0200

    Change tests

--- src/lambda/routes/game.js
+++ src/lambda/routes/game.js
@@ -1,9 +1,13 @@
 import Router from "express-promise-router";
+import axios from "axios";
 
 const router = new Router();
 // Exportem el router
 export default router;
 
 router.get("/", async (req, res) => {
-  res.send({ msg: "Test ok" });
+  const response = await axios.get("https://opensky-network.org/api/states/all")
+  const data = response.data
+
+  res.send({ msg: JSON.stringify(data,2) });
 });
