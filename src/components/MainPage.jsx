18e57158687c2ebc7ec8a56cd5405c8f7a058581
RogerSG11 <rogerserrat11@gmail.com>
2019-10-12 20:32:50 +0200

    Ground and cloud added to main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,7 +5,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import { IconButton } from "@material-ui/core";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import Sun from "../images/sun.svg";
+import Ground from "../images/ground.svg";
 import { withRouter } from "react-router-dom";
+import Cloud from "../images/cloud.svg";
 
 const useStyles = makeStyles(theme => ({
   main: {
@@ -30,6 +32,26 @@ const useStyles = makeStyles(theme => ({
   },
   bigFont: {
     fontSize: "10rem"
+  },
+  groundContainer: {
+    position: "fixed",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: "100%",
+    padding: "0px"
+  },
+  groundImg: {
+    width: "100%",
+    margin: "0px",
+    padding: "0px",
+    marginBottom: "-20px"
+  },
+  cloudContainer: {
+    position: "fixed",
+    top: 0,
+    right: 0,
+    zIndex: 1
   }
 }));
 
@@ -38,6 +60,9 @@ const MainPage = ({ history, ...props }) => {
 
   return (
     <div className={classes.main}>
+      <div className={classes.cloudContainer}>
+        <img src={Cloud} alt="Cloud" />
+      </div>
       <Typography variant="h3" className={classes.titleText}>
         Where's my <b>API</b>?
       </Typography>
@@ -47,12 +72,10 @@ const MainPage = ({ history, ...props }) => {
           onClick={() => history.push("/gateselection")}
         >
           <img src={Sun} height="250px" width="250px" alt="Sun" />
-          {/* <PlayArrowIcon
-            fontSize="large"
-            className={classes.playIcon}
-            classes={{ fontSizeLarge: classes.bigFont }}
-          /> */}
         </IconButton>
+      </div>
+      <div className={classes.groundContainer}>
+        <img className={classes.groundImg} src={Ground} alt="Mountain"/>
       </div>
     </div>
   );
