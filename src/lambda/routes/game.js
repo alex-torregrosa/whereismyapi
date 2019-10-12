a2200cf3b1c0409ddf1b0ca9ab1116b7697258ae
Pep Borrell <borrellpep@gmail.com>
2019-10-12 03:09:51 +0200

    un regalet

--- src/lambda/routes/game.js
+++ src/lambda/routes/game.js
@@ -5,9 +5,21 @@ const router = new Router();
 // Exportem el router
 export default router;
 
+var randomPlane = (l) => {
+  let n = Math.random()*l
+  return Math.floor(n)
+}
+
 router.get("/", async (req, res) => {
   const response = await axios.get("https://opensky-network.org/api/states/all")
-  const data = response.data
+  const data = response.data["states"];
+  let n = randomPlane(data.length);
+  while (data[n][8] != false) {
+    n = randomPlane(data.length);
+  }
+  var plane_info = {};
+  plane_info.icao24 = data[n][0]
+  plane_info.lonlat = [data[n][5], data[n][6]];
 
-  res.send({ msg: JSON.stringify(data,2) });
+  res.send(plane_info);
 });
