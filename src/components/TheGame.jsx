17ace1a4ef6fa239790b4b283d945f6c1f68501b
Àlex Torregrosa <alextorre98@gmail.com>
2019-10-12 13:30:23 +0200

    New Score

--- src/components/TheGame.jsx
+++ src/components/TheGame.jsx
@@ -9,6 +9,8 @@ import { Map, TileLayer, Marker } from "react-leaflet";
 import { WhiteCircularProgress } from "./utils/WhiteComponents";
 import db from "../lambda/lib/firebase";
 import { IconArrival, IconDeparture, IconPlane } from "./utils/icons";
+import { computeScore } from "./utils/scoreUtils";
+
 const defaultTheme = createMuiTheme();
 
 const useStyles = makeStyles(theme => ({
@@ -71,7 +73,12 @@ const TheGame = ({ history, gate, gameState, ...props }) => {
           // Submit score
           const dir = db.ref("/users/" + gate + "/" + cuid());
           console.log([clickPtr.lat, clickPtr.lng]);
-          dir.set([clickPtr.lat, clickPtr.lng]);
+          dir.set(
+            computeScore(
+              [clickPtr.lat, clickPtr.lng],
+              [gameState.latitude, gameState.longitude]
+            )
+          );
           history.push("/");
         }
       }
